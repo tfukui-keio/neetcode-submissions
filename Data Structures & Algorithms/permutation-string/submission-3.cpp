ce2a4b4c09634450ce3514cea5ca0e5c943c0da2
toshiki_fukui_keio <73146285+tfukui-keio@users.noreply.github.com>
2026-04-13 12:34:16 +0900

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int left{0};
+        std::unordered_map<char, int> map_s1;
+        for (char c:s1) {
+            map_s1[c]++;
+        }
+        std::unordered_map<char,int> map_s2;
+        for (int right{0};right < s2.length(); right++) {
+            if (right-left+1 > s1.length())  {
+                map_s2[s2[left]]--;
+                if (map_s2[s2[left]] == 0) {
+                    map_s2.erase(s2[left]);
+                }
+                left++;
+            }
+            map_s2[s2[right]]++;
+            std::cout << std::endl;
+            if (map_s1 == map_s2) return true;
+        }
+        return false;
+    }
+};
