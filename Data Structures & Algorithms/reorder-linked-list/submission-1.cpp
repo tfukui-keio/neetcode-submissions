fea17e6f450488bac53b0c0470ae901d8992e060
toshiki_fukui_keio <73146285+tfukui-keio@users.noreply.github.com>
2026-04-19 20:36:33 +0900

    Add: reorder-linked-list - submission-1

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-1.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        // copy pointer to avoid changing of entry point pointer.
+        std::vector<ListNode*> nodes;
+        ListNode* curr{head};
+        while(curr) {
+            nodes.push_back(curr);
+            curr = curr->next;
+        }
+
+        int left{0}; int right{(int)nodes.size()-1};
+        while (left < right) {
+            nodes[left]->next = nodes[right];
+            left++;
+
+            if (left==right) break;
+            nodes[right]->next = nodes[left];
+            right--;
+        }
+        nodes[left]->next = nullptr; // これがないと無限ループになるので、nullptrセットが重要
+    }
+};
